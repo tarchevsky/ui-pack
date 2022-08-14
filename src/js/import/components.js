ab4020a4524001b622f73be14784533fdd45aca2
Иван <i.tarchevsky@gmail.com>
2022-08-14 11:49:26 +0300

    Подключение базовых компонентов и немного ещё для демо и доработки

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -8,11 +8,12 @@ import "%components%/modal/modal";
 
 //! Other
 
-// import "%components%/accordion/accordion";
+import "%components%/accordion/accordion";
+import "%components%/show-more/show-more";
+import "%components%/slider/slider";
+import "%components%/tabs-tags/tabs-tags";
+
 // import "%components%/dropdown-lang/dropdown-lang";
-// import "%components%/show-more/show-more";
-// import "%components%/slider/slider";
-// import "%components%/tabs-tags/tabs-tags";
 // import "%components%/breadcrumbs/breadcrumbs";
 // import "%components%/circle/circle";
 // import "%components%/quiz/quiz";
