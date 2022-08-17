84c948a982f90e0278a543f689865993cc3f6bf2
Иван <i.tarchevsky@gmail.com>
2022-08-17 18:58:03 +0300

    Скрытие всех необязательных компонентов (стили, скрипты)

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -4,14 +4,13 @@ import "%components%/burger/burger";
 
 //! Other
 
-import "%components%/dropdown/dropdown";
-import "%components%/modal/modal";
-import "%components%/gallery/gallery";
-import "%components%/slider/slider";
-import "%components%/accordion/accordion";
-import "%components%/show-more/show-more";
-import "%components%/tabs-tags/tabs-tags";
-
+// import "%components%/dropdown/dropdown";
+// import "%components%/modal/modal";
+// import "%components%/gallery/gallery";
+// import "%components%/slider/slider";
+// import "%components%/accordion/accordion";
+// import "%components%/show-more/show-more";
+// import "%components%/tabs-tags/tabs-tags";
 // import "%components%/dropdown-lang/dropdown-lang";
 // import "%components%/breadcrumbs/breadcrumbs";
 // import "%components%/circle/circle";
