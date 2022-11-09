5324822b4f307fff7b6d2130648229e8ce5fe709
Иван <i.tarchevsky@gmail.com>
2022-11-09 19:55:52 +0300

    init: в img.js добавлен тот случай, если картинка не нуждается в ленивой загрузке - data-src. Тогда не будет ошибки и скрипт не будет пытаться сформировать новый путь

--- src/blocks/components/img/img.js
+++ src/blocks/components/img/img.js
@@ -1,10 +1,12 @@
 const imageObserver = new IntersectionObserver(
-    (entries,options) => {
+    (entries, options) => {
         entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.src = entry.target.dataset.src;
-                entry.target.alt = entry.target.dataset.alt;
-                imageObserver.unobserve(entry.target);
+            if (entry.target.dataset.src !== undefined) {
+                if (entry.isIntersecting) {
+                    entry.target.src = entry.target.dataset.src;
+                    entry.target.alt = entry.target.dataset.alt;
+                    imageObserver.unobserve(entry.target);
+                }
             }
         });
     },
