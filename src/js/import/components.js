ace3792f1e7f9bd44b7e463b5f1d745c3a5cafab
Иван <i.tarchevsky@gmail.com>
2022-12-02 13:55:18 +0300

    refactor: удаление комментариев в объявлениях компонентов для легкости работы с библиотекой и отстутвия комметариев в css. А также удаление ion иконок из index.pug и закомментированноо объявления vendor.js

--- src/js/import/components.js
+++ src/js/import/components.js
@@ -5,19 +5,3 @@ import "%components%/burger/burger";
 
 //! Other
 
-// import "%components%/tabs/tabs";
-// import "%components%/gallery/gallery";
-// import "%components%/flex-gallery/flex-gallery";
-// import "%components%/slider/slider";
-// import "%components%/accordion/accordion";
-// import "%components%/show-more/show-more";
-// import "%components%/tabs-tags/tabs-tags";
-// import "%components%/circle/circle";
-// import "%components%/dropdown/dropdown";
-
-// import "%components%/infinite-scroll/infinite-scroll";
-// import "%components%/quiz/quiz";
-// import "%components%/like/like";
-// import "%components%/vertical-slider/vertical-slider";
-// import "%components%/dropdown-lang/dropdown-lang";
-// import "%components%/drag-n-drop/drag-n-drop";
