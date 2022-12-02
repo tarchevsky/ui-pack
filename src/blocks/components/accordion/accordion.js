45d9d7eec57a020b22053936ce94c355957fb548
Иван <i.tarchevsky@gmail.com>
2022-12-02 11:19:12 +0300

    refactor: компонент accordion перепись всего. details не прокатил, работает криво и некрасиво. Сделал по аналогии c show-more, удалось минимизировать код и написать несколько лаконичных функций.

--- src/blocks/components/accordion/accordion.js
+++ src/blocks/components/accordion/accordion.js
@@ -1,7 +1,61 @@
-const accordionHeading = document.querySelectorAll(".accordion__heading");
+const accordion = document.querySelector(".accordion"),
+    accordionHeading = document.querySelectorAll(".accordion__heading"),
+    accordionIcon = document.querySelectorAll(".accordion__icon"),
+    accordionContent = document.querySelectorAll(".accordion__content");
 
-accordionHeading.forEach((event) => {
-    event.addEventListener("click", () => {
-        event.querySelector(".accordion__icon").classList.toggle("accordion__icon--active");
+if(accordion) {
+    accordionForEach();
+    window.addEventListener("resize", () => resizeContent());
+}
+
+function accordionForEach() {
+    accordionHeading.forEach((header, i) => {
+        header.addEventListener("click", () => { accordionBasic(i, accordionHeading, accordionIcon, accordionContent, "accordion__heading--active", "accordion__icon--active", "accordion__content--active"); });
     });
-});
+}
+
+function accordionSetTimeot(item, value) {
+    setTimeout(() => {
+        item.style.display = value;
+    }, 1000);
+}
+
+function accordionBasic(i, heading, icon, content, headingActive, iconActive, contentActive) {
+    if (!heading[i].classList.contains(headingActive)) {
+        content.forEach(item => {
+            item.classList.remove(contentActive);
+            item.style.maxHeight = "";
+            accordionSetTimeot(item, "");
+        });
+        content.forEach(item => {
+            item.classList.remove(contentActive);
+            item.style.maxHeight = "";
+        });
+        icon.forEach(item => {
+            item.classList.remove(iconActive);
+        });
+        heading.forEach((item) => {
+            item.classList.remove(headingActive);
+        });
+        heading[i].classList.add(headingActive);
+        icon[i].classList.add(iconActive);
+        content[i].classList.add(contentActive);
+        accordionSetTimeot(content[i], "block");
+        content[i].style.display = "block";
+        content[i].style.maxHeight = `${content[i].scrollHeight}px`;
+    } else {
+        icon[i].classList.remove(iconActive);
+        content[i].style.maxHeight = "";
+        accordionSetTimeot(content[i], "");
+        content[i].classList.remove(contentActive);
+        heading[i].classList.remove(headingActive);
+    }
+}
+
+function resizeContent() {
+    const accordionContentActive = document.querySelector(".accordion__content--active");
+
+    if (parseInt(accordionContentActive.style.maxHeight) !== accordionContentActive.scrollHeight) {
+        accordionContentActive.style.maxHeight = `${accordionContentActive.scrollHeight}px`;
+    }
+}
