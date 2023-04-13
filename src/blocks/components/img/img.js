0e1a327e5c429e5906f5c49a06561b0f4c5d6e07
Иван <i.tarchevsky@gmail.com>
2023-04-13 17:52:05 +0300

    refactor + feat: изменение принципа работы img - теперь он вместо контейнера .img в picture для использования webp одновременно с jpg (для safari). Также обновление скрипта ленивой загрузки и изменения атрибутов. Добавлен миксин pug - picture и дописана документация

--- src/blocks/components/img/img.js
+++ src/blocks/components/img/img.js
@@ -1,16 +1,25 @@
-const imageObserver = new IntersectionObserver(
-    (entries, options) => {
-        entries.forEach((entry) => {
-            if (entry.target.dataset.src !== undefined) {
-                if (entry.isIntersecting) {
-                    entry.target.src = entry.target.dataset.src;
-                    entry.target.alt = entry.target.dataset.alt;
-                    imageObserver.unobserve(entry.target);
-                }
-            }
-        });
-    },
-    {}
-);
+const imageObserver = new IntersectionObserver((entries, options) => {
+	entries.forEach(entry => {
+		if (entry.target.dataset.src !== undefined) {
+			if (entry.isIntersecting) {
+				entry.target.src = entry.target.dataset.src
+				entry.target.alt = entry.target.dataset.alt
+				imageObserver.unobserve(entry.target)
+			}
+		}
 
-let images = document.querySelectorAll('img').forEach(image => imageObserver.observe(image));
+		if (entry.target.dataset.srcset !== undefined) {
+			if (entry.isIntersecting) {
+				entry.target.srcset = entry.target.dataset.srcset
+				imageObserver.unobserve(entry.target)
+			}
+		}
+	})
+}, {})
+
+let images = document
+		.querySelectorAll('img')
+		.forEach(image => imageObserver.observe(image)),
+	altImages = document
+		.querySelectorAll('source')
+		.forEach(alt => imageObserver.observe(alt))
