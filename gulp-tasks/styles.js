f1f896158f62e483e3cf5a93a5bc1b82c5b05c51
Иван <i.tarchevsky@gmail.com>
2023-04-13 13:51:25 +0300

    refactor: внедрение другой обработки ошибок в sass через plumber - теперь сборку не нужно будет перезапускать после ошибки. А также автоматическая сортировка импортов и удаление лишних знаков

--- gulp-tasks/styles.js
+++ gulp-tasks/styles.js
@@ -1,59 +1,82 @@
-"use strict";
+'use strict'
 
-import { paths } from "../gulpfile.babel";
-import gulp from "gulp";
-import gulpif from "gulp-if";
-import rename from "gulp-rename";
-import dartsass from "sass";
-import gulpsass from "gulp-sass";
-import mincss from "gulp-clean-css";
-import groupmedia from "gulp-group-css-media-queries";
-import autoprefixer from "gulp-autoprefixer";
-import sourcemaps from "gulp-sourcemaps";
-import plumber from "gulp-plumber";
-import browsersync from "browser-sync";
-import debug from "gulp-debug";
-import yargs from "yargs";
+import browsersync from 'browser-sync'
+import gulp from 'gulp'
+import autoprefixer from 'gulp-autoprefixer'
+import mincss from 'gulp-clean-css'
+import debug from 'gulp-debug'
+import groupmedia from 'gulp-group-css-media-queries'
+import gulpif from 'gulp-if'
+import plumber from 'gulp-plumber'
+import rename from 'gulp-rename'
+import gulpsass from 'gulp-sass'
+import sourcemaps from 'gulp-sourcemaps'
+import dartsass from 'sass'
+import yargs from 'yargs'
+import { paths } from '../gulpfile.babel'
 
-const sass = gulpsass(dartsass);
+const sass = gulpsass(dartsass)
 const argv = yargs.argv,
-    production = !!argv.production;
+	production = !!argv.production
 
-gulp.task("styles", () => {
-    return gulp.src(paths.styles.src)
-        .pipe(gulpif(!production, sourcemaps.init()))
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(groupmedia())
-        .pipe(gulpif(production, autoprefixer({
-            cascade: false,
-            grid: true
-        })))
-        .pipe(gulpif(production, mincss({
-            compatibility: "ie8", level: {
-                1: {
-                    specialComments: 0,
-                    removeEmpty: true,
-                    removeWhitespace: true
-                },
-                2: {
-                    mergeMedia: true,
-                    removeEmpty: true,
-                    removeDuplicateFontRules: true,
-                    removeDuplicateMediaBlocks: true,
-                    removeDuplicateRules: true,
-                    removeUnusedAtRules: false
-                }
-            }
-        })))
-        .pipe(gulpif(production, rename({
-            suffix: ".min"
-        })))
-        .pipe(plumber.stop())
-        .pipe(gulpif(!production, sourcemaps.write("./maps/")))
-        .pipe(gulp.dest(paths.styles.dist))
-        .pipe(debug({
-            "title": "CSS files"
-        }))
-        .on("end", browsersync.reload);
-});
+gulp.task('styles', () => {
+	return gulp
+		.src(paths.styles.src)
+		.pipe(gulpif(!production, sourcemaps.init()))
+		.pipe(
+			plumber(function (error) {
+				console.error(error.messageFormatted)
+				this.emit('end')
+			})
+		)
+		.pipe(sass())
+		.pipe(groupmedia())
+		.pipe(
+			gulpif(
+				production,
+				autoprefixer({
+					cascade: false,
+					grid: true,
+				})
+			)
+		)
+		.pipe(
+			gulpif(
+				production,
+				mincss({
+					compatibility: 'ie8',
+					level: {
+						1: {
+							specialComments: 0,
+							removeEmpty: true,
+							removeWhitespace: true,
+						},
+						2: {
+							mergeMedia: true,
+							removeEmpty: true,
+							removeDuplicateFontRules: true,
+							removeDuplicateMediaBlocks: true,
+							removeDuplicateRules: true,
+							removeUnusedAtRules: false,
+						},
+					},
+				})
+			)
+		)
+		.pipe(
+			gulpif(
+				production,
+				rename({
+					suffix: '.min',
+				})
+			)
+		)
+		.pipe(gulpif(!production, sourcemaps.write('./maps/')))
+		.pipe(gulp.dest(paths.styles.dist))
+		.pipe(
+			debug({
+				title: 'CSS files',
+			})
+		)
+		.on('end', browsersync.reload)
+})
