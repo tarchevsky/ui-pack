1dedcc3e670d00253b312e6cddf3d2aabc23208f
Иван <i.tarchevsky@gmail.com>
2023-04-13 20:08:03 +0300

    refactor: перепись скрипта imageOvserver, сокращение кода, разделение picture на два миксина - picture и picture-lazy, написание документации

--- src/blocks/components/img/img.js
+++ src/blocks/components/img/img.js
@@ -1,25 +1,29 @@
 const imageObserver = new IntersectionObserver((entries, options) => {
 	entries.forEach(entry => {
-		if (entry.target.dataset.src !== undefined) {
-			if (entry.isIntersecting) {
+		if (entry.isIntersecting) {
+			if (
+				entry.target.dataset.src !== undefined &&
+				entry.target.dataset.src !== null
+			) {
 				entry.target.src = entry.target.dataset.src
-				entry.target.alt = entry.target.dataset.alt
-				imageObserver.unobserve(entry.target)
 			}
-		}
-
-		if (entry.target.dataset.srcset !== undefined) {
-			if (entry.isIntersecting) {
+			if (
+				entry.target.dataset.srcset !== undefined &&
+				entry.target.dataset.srcset !== null
+			) {
 				entry.target.srcset = entry.target.dataset.srcset
-				imageObserver.unobserve(entry.target)
 			}
+			if (
+				entry.target.dataset.alt !== undefined &&
+				entry.target.dataset.alt !== null
+			) {
+				entry.target.alt = entry.target.dataset.alt
+			}
+			imageObserver.unobserve(entry.target)
 		}
 	})
 }, {})
 
-let images = document
-		.querySelectorAll('img')
-		.forEach(image => imageObserver.observe(image)),
-	altImages = document
-		.querySelectorAll('source')
-		.forEach(alt => imageObserver.observe(alt))
+document
+	.querySelectorAll('img, source')
+	.forEach(el => imageObserver.observe(el))
